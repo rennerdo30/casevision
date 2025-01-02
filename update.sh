338c4b1d2f1ae37077659fbdcc15007641964d52
rennerdo30 <github@renner.dev>
2025-01-02 22:48:29 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,4 +5,6 @@
 git config --global --add safe.directory /opt/casevision
 git pull https://github.com/rennerdo30/casevision.git
 
-
+sudo systemctl daemon-reload
+sudo systemctl restart casevision-playback
+sudo systemctl restart casevision-control
